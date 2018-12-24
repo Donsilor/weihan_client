9c75c004c37393742dcee33697897eff27a9a60e
mrli <asd995633088@qq.com>
2018-12-24 14:40:59 +0800

    的撒

--- net_manager/src/common/serviceurls.js
+++ net_manager/src/common/serviceurls.js
@@ -2,26 +2,28 @@
 import { HTTP_REQUEST_METHOD, UPDATE_FILE_HOST } from "./constants";
 
 export default {
-  operator: {
-    getlist: {
-      /**接口地址 */
-      url: '/operator/getlist',
-      /**接口方法 */
-      method: HTTP_REQUEST_METHOD.POST,
-      /**请求参数 */
-      param: { b: 234 },
-      /**填写Token的字符串Key */
-      authorization: false,
-      /**虚拟服务： */
-      virtual_service: true,
-      /**虚拟控制器：当虚拟服务被开启的时候，请求将不会真正的发起，而是发送到这个虚拟控制器中来，由虚拟控制器模拟后端操作，从而在业务逻辑确定的情况下可以与后端同步开发 */
-      virtual_controller(params) {
-        return this.responseBody(params, "成功")
-      }
-    },
-  },
+  // operator: {
+  //   getlist: {
+  //     /**接口地址 */
+  //     url: '/operator/getlist',
+  //     /**接口方法 */
+  //     method: HTTP_REQUEST_METHOD.POST,
+  //     /**请求参数 */
+  //     param: { b: 234 },
+  //     /**填写Token的字符串Key */
+  //     authorization: false,
+  //     /**虚拟服务： */
+  //     virtual_service: true,
+  //     /**虚拟控制器：当虚拟服务被开启的时候，请求将不会真正的发起，而是发送到这个虚拟控制器中来，由虚拟控制器模拟后端操作，从而在业务逻辑确定的情况下可以与后端同步开发 */
+  //     virtual_controller(params) {
+  //       return this.responseBody(params, "成功")
+  //     }
+  //   },
+  // },
   service: {
+    /**实操 */
     practical: {
+      /**任务 */
       task: {
         search: {
           /**接口地址 */
@@ -51,6 +53,7 @@ export default {
           method: HTTP_REQUEST_METHOD.POST,
         },
       },
+      /**考试 */
       examination: {
         search: {
           /**接口地址 */
@@ -80,16 +83,18 @@ export default {
           method: HTTP_REQUEST_METHOD.POST,
         },
       },
+      /**成绩 */
       schoolReport: {
         search: {
           /**接口地址 */
           url: '/service/practical/schoolReport/$taskId',
           /**接口方法 */
           method: HTTP_REQUEST_METHOD.GET,
-        },
+        }
       },
       /**竞赛*/
       competition: {
+        /**小组 */
         group: {
           upsert: {
             /**接口地址 */
@@ -106,16 +111,35 @@ export default {
         }
       }
     },
+    /**用户 */
     users:{
+      /**登陆 */
       login:{
         /**接口地址 */
         url: '/service/users/login',
         /**接口方法 */
         method: HTTP_REQUEST_METHOD.POST,
       }
+    },
+    /**学校 */
+    schools:{
+      search:{
+        /**接口地址 */
+        url: '/service/schools',
+        /**接口方法 */
+        method: HTTP_REQUEST_METHOD.GET,
+      },
+      insert:{
+        /**接口地址 */
+        url: '/service/schools',
+        /**接口方法 */
+        method: HTTP_REQUEST_METHOD.POST,
+      }
     }
   },
+  /**公共 */
   common: {
+    /**标签 */
     uiLabels: {
       /**接口地址 */
       url: '/common/uiLabels/$type',
